017dec63c4f12c2acbd072484b39216daf57f290
waliaga <162710089+waliaga@users.noreply.github.com>
2024-05-03 12:19:36 -0400

    add src1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  url: 'http://be-proveedores.oep.org.bo/',
+  storage: 'http://be-proveedores.oep.org.bo/public/storage/attachments/',
+  api: '/api/',
+  password: ';ohk|u-s|{[+-/.*|@]}',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  url: 'http://127.0.0.1:8000',
+  storage: 'http://127.0.0.1:8000/storage/attachments/',
+  api: '/api/',
+  password: ';ohk|u-s|{[+-/.*|@]}',
+};
